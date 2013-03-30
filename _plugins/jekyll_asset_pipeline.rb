368259440633eac38a26904d210f02f8f28033df
draug3n <draugen@draugrheim.net>
2013-03-31 00:10:25 +0100

    Basic asset pipeline set up. Also created (empty) Gemfile.

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -12,4 +12,38 @@ module JekyllAssetPipeline
       return Sass::Engine.new(@content, syntax: :scss).render
     end
   end
+
+  class CssCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.css'
+    end
+
+    def compress
+      return YUI::CssCompressor.new.compress(@content)
+    end
+  end
+
+  class JavaScriptCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.js'
+    end
+
+    def compress
+      return YUI::JavaScriptCompressor.new(munge: true).compress(@content)
+    end
+  end
+
+  class CssTagTemplate < JekyllAssetPipeline::Template
+    def self.filetype
+      '.css'
+    end
+
+    def html
+      "<link href='/#{@path}/#{@filename}' rel='stylesheet' type='text/css' media='screen' />\n"
+    end
+  end
 end
